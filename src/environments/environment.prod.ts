9a85f57b8080ff0d6e8b56932b4268a988929529
triebawerker <micha@triebawerker.de>
2017-06-14 08:15:33 +0200

    fixes mail link, changed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_host: 'https://music-festival-planner.herokuapp.com'
+  // api_host: 'https://music-festival-planner.herokuapp.com'
+  api_host: 'http://localhost:3000'
 };
